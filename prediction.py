7095bb22e1f08ccddbf5905c73046e72439b4915
sergio2526 <sergiorubiano25@hotmail.com>
2021-04-15 15:26:24 -0500

    Agregando condicion de % de confianza

--- prediction.py
+++ prediction.py
@@ -25,13 +25,16 @@ def read_imagefile(file) -> Image.Image:
 def predict(image: Image.Image):
     image = np.asarray(image.resize((224, 224)))[..., :3]
     image = np.expand_dims(image, 0)
-    image = image / 127.5 - 1.0
-    result = decode_predictions(model.predict(image), 2)[0]
+    image = image / 255.0
+    result = decode_predictions(model.predict(image), 5)[0]
     response = []
     for i, res in enumerate(result):
         resp = {}
         resp["class"] = res[1]
-        resp["confidence"] = f"{res[2]*100:0.2f} %"
-        response.append(resp)
+        resp["confidence"] = f"{res[2]*100:0.2f}"
+        if float(resp['confidence']) >= 20.0:
+            response.append(resp)
+        else:
+            pass
 
     return response
