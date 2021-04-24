6eed16b05f9a42790ea5e0249e3053bbf0b0bf0c
sergio2526 <sergiorubiano25@hotmail.com>
2021-04-24 15:14:59 -0500

    Agregando archivos

---
+++ App/model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import os
+
+# Función que carga el modelo H5
+def loadModelH5():
+    MODEL_H5_FILE = "/home/sergio/Descargas/flowers.h5"
+    # Cargar el modelo
+    loaded_model = tf.keras.models.load_model(MODEL_H5_FILE)
+    return loaded_model
